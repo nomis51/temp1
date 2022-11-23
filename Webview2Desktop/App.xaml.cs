6facbe5a71ed6e02fb1613913b42b13b5833140e
nomis51 <nomis51@users.noreply.github.com>
2022-11-23 08:54:50 -0500

    Initial commit

---
+++ Webview2Desktop/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Webview2Desktop.WebView;
+
+namespace Webview2Desktop;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    public App()
+    {
+        JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        Exit += OnExit;
+    }
+
+    private void OnExit(object sender, ExitEventArgs e)
+    {
+        WebViewBootstrapper.Dispose();
+    }
+}
